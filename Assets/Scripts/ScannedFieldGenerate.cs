2846f2acfcc5f66ba1939695629126c1a51a25a0
B10532021 <b10532021@gapps.ntust.edu.tw>
2019-10-20 18:12:03 +0800

    做一些調整

--- Assets/Scripts/ScannedFieldGenerate.cs
+++ Assets/Scripts/ScannedFieldGenerate.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 using GoogleARCore;
-//using TriangleNet.Geometry;
-//using TriangleNet.Topology;
 using HullDelaunayVoronoi.Delaunay;
 using HullDelaunayVoronoi.Primitives;
+using DrawingTool;
 
 public class ScannedFieldGenerate : MonoBehaviour
 {
@@ -247,63 +246,3 @@ public class ScannedFieldGenerate : MonoBehaviour
     }
 }
 
-public struct LineDrawer
-{
-    private LineRenderer lineRenderer;
-    private float lineSize;
-
-    public LineDrawer(float lineSize = 0.008f)
-    {
-        GameObject lineObj = new GameObject("LineObj");
-        lineRenderer = lineObj.AddComponent<LineRenderer>();
-        //Particles/Additive
-        lineRenderer.material = new Material(Shader.Find("Hidden/Internal-Colored"));
-
-        this.lineSize = lineSize;
-    }
-
-    private void init(float lineSize = 0.008f)
-    {
-        if (lineRenderer == null)
-        {
-            GameObject lineObj = new GameObject("LineObj");
-            lineRenderer = lineObj.AddComponent<LineRenderer>();
-            //Particles/Additive
-            lineRenderer.material = new Material(Shader.Find("Hidden/Internal-Colored"));
-
-            this.lineSize = lineSize;
-        }
-    }
-
-    //Draws lines through the provided vertices
-    public void DrawLineInGameView(Vector3 start, Vector3 end, Color color)
-    {
-        if (lineRenderer == null)
-        {
-            init(0.008f);
-        }
-
-        //Set color
-        lineRenderer.startColor = color;
-        lineRenderer.endColor = color;
-
-        //Set width
-        lineRenderer.startWidth = lineSize;
-        lineRenderer.endWidth = lineSize;
-
-        //Set line count which is 2
-        lineRenderer.positionCount = 2;
-
-        //Set the postion of both two lines
-        lineRenderer.SetPosition(0, start);
-        lineRenderer.SetPosition(1, end);
-    }
-
-    public void Destroy()
-    {
-        if (lineRenderer != null)
-        {
-            UnityEngine.Object.Destroy(lineRenderer.gameObject);
-        }
-    }
-}
